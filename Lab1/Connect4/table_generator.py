feec601b86cc3607d6096788cce6c611633d9187
Admin <admin@MacBook-Pro-de-Admin.local>
2020-09-07 15:39:43 +0200

    table generator corrigé

--- Lab1/Connect4/table_generator.py
+++ Lab1/Connect4/table_generator.py
@@ -1,11 +1,11 @@
 import random
 
+
 def generate_table():
-    sum = 6*7
-    tableau = [[0 for j in range(6)] for i in range(7)]
+    prio = [i % 7 + 1 for i in range(6*7)]
+    random.shuffle(prio)
+    tableau = [[j for j in range(6)] for i in range(7)]
     for i in range(7):
         for j in range(6):
-            r = random.random()*sum
-            tableau[i][j] = r
-            sum -= r
+            tableau[i][j] = prio.pop()
     return tableau
